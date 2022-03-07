3d3e3df920533129fbceb2af05ae3a0811609663
Terry Chia <terry@ayrx.me>
2022-03-07 14:08:14 +0800

    Calculate subject_hash at build time.

---
+++ build.rs
@@ -0,0 +1,33 @@
+use std::env;
+use md5::{Digest, Md5};
+use std::fs;
+use std::path::Path;
+use x509_parser::prelude::*;
+
+fn main() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("hash.rs");
+
+    let cert_content = fs::read("certs/ca_cert.der").unwrap();
+    let hash = calculate_subject_hash(&cert_content);
+
+    fs::write(
+        &dest_path,
+        format!("const SUBJECT_HASH: &str = \"{}\";", hash)
+    ).unwrap();
+
+    println!("cargo:rerun-if-changed=certs/ca_cert.der");
+}
+
+fn calculate_subject_hash(cert: &[u8]) -> String {
+    let (_rem, x509) = X509Certificate::from_der(cert).unwrap();
+
+    let mut hasher = Md5::new();
+    hasher.update(x509.subject().as_raw());
+    let hash = hasher.finalize();
+
+    let mut h = hash[0..4].to_vec();
+    h.reverse();
+
+    hex::encode(&h)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use anyhow::Result;
-use md5::{Digest, Md5};
 use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
 use std::process::Command;
-use x509_parser::prelude::*;
+
+include!(concat!(env!("OUT_DIR"), "/hash.rs"));
 
 fn main() -> Result<()> {
     let ca_cert_bytes = include_bytes!("../certs/ca_cert.der");
-    let subject_hash = calculate_subject_hash(ca_cert_bytes)?;
+    // let subject_hash = calculate_subject_hash(ca_cert_bytes)?;
 
     // Save all content of the cacerts directory.
     let mut certs = HashMap::new();
@@ -32,7 +32,7 @@ fn main() -> Result<()> {
     }
 
     // Write the new certificate to the cacerts directory.
-    fs::write(base.join(format!("{}.0", subject_hash)), ca_cert_bytes)?;
+    fs::write(base.join(format!("{}.0", SUBJECT_HASH)), ca_cert_bytes)?;
 
     // Ensure that all contents of the cacerts directory have the appropriate
     // permissions.
@@ -52,18 +52,4 @@ fn main() -> Result<()> {
         .status()?;
 
     Ok(())
-}
-
-fn calculate_subject_hash(cert: &[u8]) -> Result<String> {
-    let (_rem, x509) = X509Certificate::from_der(cert)?;
-
-    let mut hasher = Md5::new();
-    hasher.update(x509.subject().as_raw());
-    let hash = hasher.finalize();
-
-    let mut h = hash[0..4].to_vec();
-    h.reverse();
-    let subject_hash = hex::encode(&h);
-
-    Ok(subject_hash)
 }
